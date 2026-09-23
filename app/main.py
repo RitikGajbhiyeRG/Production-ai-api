cede88c7606b8ca57324a5b44e1d310adbe60f94
Ritik Gajbhiye <ritikgajbhiye99@gmail.com>
2026-09-24 02:22:39 +0530

    Add Environment Configuration

--- app/main.py
+++ app/main.py
@@ -1,7 +1,19 @@
+import dotenv
 from fastapi import FastAPI
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app=FastAPI()
 
+
+app_name=os.getenv("APP_NAME","Production AI API")
+
+environment=os.getenv("ENVIRONMENT","development")
+
+
 @app.get("/")
 def home():
-    return {"MEssage : production app is running !!!"}
+    return {"MEssage": "production app is running !!!",
+    "environment": environment}
